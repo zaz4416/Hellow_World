3bbdc9393b8caddacc685e9dcb7d263c67a64c2a
zaz4416 <kaguyazxc2@gmail.com>
2026-01-25 23:29:23 +0900

    Initial commit

---
+++ Panele_Hellow_World.jsx
@@ -0,0 +1,166 @@
+﻿/*
+<javascriptresource>
+<name>Hello World</name>
+</javascriptresource>
+*/
+
+/*----------------------------------------------------------
+    CPaletteWindowをつかって、”Hlloe world”
+
+    https://scriptui.joonas.me/
+      ↑ Script Bulderで生成されwたスクリプトをそのまま使用できます
+------------------------------------------------------------*/
+
+/*
+ CPaletteWindowを基底クラスとして作成しました。
+ ここでは、CHellowWorldDlgに継承させて、ボタンがひとつだけのダイアログを表示して、
+ このボタンを押すことで、Hellow Worldと表示して終わります。
+
+ ボタンが押されたが押された後の処理はおおまかには、下記のようになっています。
+   ボタンが押された　→　onClick　→　CallFuncでBridgeTalkを使用してSayHelloWorldを呼ぶ　→　HelloWorldを呼ぶ
+*/
+
+// Ver.1.0 : 2026/01/25
+
+#target illustrator
+#targetengine "main"
+
+SELF = (function(){
+    try {app.documents.test()}
+    catch(e) {return File(e.fileName)}
+})();
+
+// 外部のJSXを読み込む
+$.evalFile(SELF.path + "/ZazLib/" + "PaletteWindow.jsx");
+
+
+// 言語ごとの辞書を定義
+var LangStrings = {
+    GUI_JSX: {
+        en : "ScriptUI Dialog Builder - Export_EN.jsx",
+        ja : "ScriptUI Dialog Builder - Export_JP.jsx"
+    },
+    hello_world: {
+        en: "Hello world",
+        ja: "こんにちは世界"
+    },
+    boy: {
+        en: "I'm boy",
+        ja: "私は男の子です"
+    },
+    girl: {
+        en: "I'm girl",
+        ja: "私は女の子です"
+    },
+};
+
+
+//-----------------------------------
+// クラス CHuman
+//-----------------------------------
+
+// 1. コンストラクタ定義
+function CHuman() {
+}
+
+// 2. プロトタイプメソッドの定義
+CHuman.prototype.HayHelloAnyone = function( Anyone ) {
+    alert(localize(LangStrings.hello_world) + "\n" + Anyone );  // BridgeTalk後に呼ばれるので、localize()が必要
+}
+
+
+//-----------------------------------
+// クラス CBoy
+//-----------------------------------
+
+// 1. コンストラクタ定義
+function CBoy() {
+    CHuman.call(this);  // 親のプロパティを継承
+}
+
+// 2. クラス継承
+ClassInheritance(CBoy, CHuman);
+
+// 3. プロトタイプメソッドの定義
+CBoy.prototype.HayHello = function() {
+    this.HayHelloAnyone( localize(LangStrings.boy) );   // BridgeTalk後に呼ばれるので、localize()が必要
+}
+
+
+//-----------------------------------
+// クラス CGirl
+//-----------------------------------
+
+// 1. コンストラクタ定義
+function CGirl() {
+    CHuman.call(this); // 親のプロパティを継承
+} // コンストラクタ (ここまで) 
+
+// 2. クラス継承
+ClassInheritance(CGirl, CHuman);
+
+// 3. プロトタイプメソッドの定義
+CGirl.prototype.HayHello = function() {
+    this.HayHelloAnyone( localize(LangStrings.girl) );  // BridgeTalk後に呼ばれるので、localize()が必要
+}
+
+
+//-----------------------------------
+// クラス CHelloWorldDlg
+//-----------------------------------
+
+// 1. コンストラクタ定義
+function CHelloWorldDlg() {
+    CPaletteWindow.call( this, false );      // コンストラクタ
+    var self = this;                         // クラスへののポインタを確保
+
+    // GUI用のスクリプトを読み込む
+    var selfFile = new File($.fileName);
+    var currentDir = selfFile.parent;
+    if ( self.LoadGUIfromJSX( currentDir.fullName + "/GUI.Panele_Hellow_World/" + LangStrings.GUI_JSX ) )
+    {
+        // GUIに変更を入れる
+        self.button1.onClick = function() { self.onSayHelloWorldClick(); }
+    }
+    else {
+        alert("GUIが未定です");
+        return;
+    }
+}
+
+// 2. クラス継承
+ClassInheritance(CHelloWorldDlg, CPaletteWindow);
+
+// 3. 静的メソッドの定義
+CHelloWorldDlg.SayHelloWorld = function() {
+    var self = CHelloWorldDlg.self;
+    self.HelloWorld( new CBoy() );
+    self.HelloWorld( new CGirl() );
+    self.CloseDlg();
+}  
+
+// 4. プロトタイプメソッドの定義
+CHelloWorldDlg.prototype.onSayHelloWorldClick = function() {
+    try
+    {
+        this.CallFunc( "SayHelloWorld" ); // 静的メソッドを呼び出すこと
+    }
+    catch(e)
+    {
+        alert( e.message );
+    }
+}
+CHelloWorldDlg.prototype.HelloWorld = function( ClassOfSomeone ) {
+    ClassOfSomeone.HayHello();
+}
+ 
+
+//インスタンスを生成。
+var DlgHello = new CHelloWorldDlg();
+
+main();
+
+function main()
+{    
+    DlgHello.ShowDlg();
+}
